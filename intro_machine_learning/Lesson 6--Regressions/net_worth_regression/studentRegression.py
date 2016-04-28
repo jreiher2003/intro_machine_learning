77ea6b350dafd16c04affb3ad7cfd0eadd2287ef
Jeff Reiher <jreiher2003@yahoo.com>
2016-04-27 23:47:19 -0400

    Fix code [[27]]

---
+++ intro_machine_learning/Lesson 6--Regressions/net_worth_regression/studentRegression.py
@@ -0,0 +1,12 @@
+def studentReg(ages_train, net_worths_train):
+    ### import the sklearn regression module, create, and train your regression
+    ### name your regression reg
+    
+    ### your code goes here!
+    from sklearn import linear_model 
+    clf = linear_model.LinearRegression() 
+    clf.fit(ages_train, net_worths_train)
+    
+    reg = clf
+    
+    return reg
