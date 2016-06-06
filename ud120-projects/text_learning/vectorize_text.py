4bccc743760a54cebf4da748db0f067eba48f95d
Jeff Reiher <jreiher2003@yahoo.com>
2016-06-06 12:42:16 -0400

    Analysis of text emails

--- ud120-projects/text_learning/vectorize_text.py
+++ ud120-projects/text_learning/vectorize_text.py
@@ -41,24 +41,32 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # temp_counter += 1
+        # if temp_counter < 20:
+        path = path.replace(".", "_")
+        path = os.path.join('..', path[:-1])
+        # print path
+        email = open(path, "r")
+        ### use parseOutText to extract the text from the opened email
+        text = parseOutText(email)
+        # print text
+        ### use str.replace() to remove any instances of the words
+        sig = ["sara", "shackleton", "chris", "germani"]
+        for word in sig:
+            if word in text:
+                text = text.replace(word, "")
+        ### append the text to word_data
+        word_data.append(text)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name == "sara":
+            from_data.append(0)
+        if name == "chris":
+            from_data.append(1)
+        email.close()
 
-            ### use parseOutText to extract the text from the opened email
-
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
-
+print len(word_data)
+# print word_data[0]
+# print from_data
 print "emails processed"
 from_sara.close()
 from_chris.close()
@@ -71,5 +79,13 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+### text vectorization--go from strings to lists of numbers
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+# print dir(vectorizer)
+# print vectorizer
+vectorizer.fit_transform(word_data)
+print len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[34597]
 
 
